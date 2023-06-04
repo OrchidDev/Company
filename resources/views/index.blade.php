41e9cb8868df15f944f5a9d10362121c04b45394
OrchidDev <swordart2050@gmail.com>
2023-06-04 15:18:03 +0330

    Creat Team Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,4 +4,5 @@
     @include('partials.service')
     @include('partials.features')
     @include('partials.pricing')
+    @include('partials.team')
 </x-app-layout>
---
+++ resources/views/partials/team.blade.php
@@ -0,0 +1,73 @@
+<section class="bg-body-secondary p-3">
+    <div class="text-center">
+        <h2 class="fw-bold mt-3 mb-3">تیم ما</h2>
+
+        <div class="row">
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="mb-4">
+                            <i class="fa-light fa-user fa-5x"></i>
+                        </div>
+                        <h2 class="fs-4 fw-bold">رایموند</h2>
+                        <div class="mt-3">
+                            <i class="fa-brands fa-telegram fa-2x"></i>
+                            <i class="fa-brands fa-linkedin fa-2x"></i>
+                            <i class="fa-brands fa-instagram fa-2x"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="mb-4">
+                            <i class="fa-light fa-user fa-5x"></i>
+                        </div>
+                        <h2 class="fs-4 fw-bold">رایموند</h2>
+                        <div class="mt-3">
+                            <i class="fa-brands fa-telegram fa-2x"></i>
+                            <i class="fa-brands fa-linkedin fa-2x"></i>
+                            <i class="fa-brands fa-instagram fa-2x"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="mb-4">
+                            <i class="fa-light fa-user fa-5x"></i>
+                        </div>
+                        <h2 class="fs-4 fw-bold">رایموند</h2>
+                        <div class="mt-3">
+                            <i class="fa-brands fa-telegram fa-2x"></i>
+                            <i class="fa-brands fa-linkedin fa-2x"></i>
+                            <i class="fa-brands fa-instagram fa-2x"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="mb-4">
+                            <i class="fa-light fa-user fa-5x"></i>
+                        </div>
+                        <h2 class="fs-4 fw-bold">رایموند</h2>
+                        <div class="mt-3">
+                            <i class="fa-brands fa-telegram fa-2x"></i>
+                            <i class="fa-brands fa-linkedin fa-2x"></i>
+                            <i class="fa-brands fa-instagram fa-2x"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</section>
