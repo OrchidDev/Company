3fb480e364e52e8fe2230726b077051b8c3dc9ee
OrchidDev <swordart2050@gmail.com>
2023-06-05 02:05:13 +0330

    Create Card Sections Sidebar

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -8,6 +8,30 @@
                 @if(auth()->user()->role === 'admin')
                 <a href="{{ route('users.index') }}" type="button" class="btn btn-light"><i class="fa-light fa-users"></i> لیست کاربران </a>
                 @endif
+                <div class="card text-bg-light mb-3">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6 col-md-8">مدیریت منو</div>
+                            <div class="col-6 col-md-4 text-center">
+                                <a href="#" class="text-decoration-none text-dark" title="ویرایش"><i class="fa-light fa-edit"></i></a>
+                                <a href="#" class="text-decoration-none text-dark" title="نمایش"><i class="fa-light fa-eye"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card text-bg-light mb-3">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6 col-md-8">مدیریت سربرگ</div>
+                            <div class="col-6 col-md-4 text-center">
+                                <a href="#" class="text-decoration-none text-dark" title="ویرایش"><i class="fa-light fa-edit"></i></a>
+                                <a href="#" class="text-decoration-none text-dark" title="نمایش"><i class="fa-light fa-eye"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
